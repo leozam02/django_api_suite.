3d5784ea39b96f4704724c1e97403a4e75e5e159
Leonardo Zambrano <151984141+leozam02@users.noreply.github.com>
2025-07-21 20:23:01 +0000

    primera parte

---
+++ homepage/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("¡Bienvenido a la aplicación Django!")
+def dashboard(request):
+    return HttpResponse("¡Bienvenido a la aplicación Django!")
